7d2fa4d846c9743a489550525f65a04c0fb42924
lightingtime <hcy19950818@gmail.com>
2018-04-12 20:08:40 +0800

    简单题了解一下

--- Leetcode/Easy/plusOne.cpp
+++ Leetcode/Easy/plusOne.cpp
@@ -20,5 +20,12 @@ vector<int> plusOne(vector<int>& digits) {
         digits.insert(digits.begin(),1);
     }
     return digits;
-} 
+}
 
+//vector<int> x={9};
+//vector<int> x={1,0};
+//vector<int> x={1,9};
+//x=plusOne(x);
+//for (int i = 0; i < x.size(); ++i) {
+//cout<<x[i];
+//}
