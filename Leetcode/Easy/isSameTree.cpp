2a3ef69a4cabd0ef23ce1cd28cd06f19903d56e7
lightingtime <hcy19950818@gmail.com>
2018-04-21 23:10:30 +0800

    递归左子树和右子树保证同时满足的结果

--- Leetcode/Easy/isSameTree.cpp
+++ Leetcode/Easy/isSameTree.cpp
@@ -5,7 +5,8 @@
 #include "easyHeader.h"
 
 bool isSameTree(TreeNode* p,TreeNode *q) {
-    if (p==NULL && q==NULL)return (p==q);
+    if(!p && !q)    return true;
+    if(!p || !q)    return false;
     //直接把左子树的结果 && 右子树的结果
     return (p->val == q->val && isSameTree(p->left,q->left) && isSameTree(p->right,q->right));
 
